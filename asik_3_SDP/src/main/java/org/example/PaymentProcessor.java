334271cdf590e020a7ea7d01e5f4a8241765ee42
Arlen20 <161277748+Arlen20@users.noreply.github.com>
2024-11-19 17:36:01 +0500

    Delete asik_3_SDP directory

--- asik_3_SDP/src/main/java/org/example/PaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class PaymentProcessor {
-    public boolean processPayment(int amount) {
-        System.out.println("Proces payment $" + amount);
-        return true;
-    }
-}
-
